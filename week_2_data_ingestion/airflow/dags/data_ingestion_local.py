ea1d14be760405c016d3812c484a8d220b0d132c
eeeds <encinaesteban27@gmail.com>
2022-05-02 19:04:02 -0400

    homework 2

--- week_2_data_ingestion/airflow/dags/data_ingestion_local.py
+++ week_2_data_ingestion/airflow/dags/data_ingestion_local.py
@@ -18,37 +18,78 @@ PG_PORT=os.getenv('PG_PORT')
 PG_DATABASE=os.getenv('PG_DATABASE')
 
 
-local_workflow = DAG(
-    "LocalIngestionDag",
+def download_transfer_dag(
+    dag,
+    url_template,
+    output_file_template,
+    table_name_template):
+
+    with dag:
+        wget_task = BashOperator(
+            task_id = 'wget',
+            bash_command = f'curl -sSLf {url_template} > {output_file_template}'
+
+        )
+        ingest_task = PythonOperator(
+            task_id = 'ingest',
+            python_callable = ingest_callable,
+            op_kwargs = dict(
+                user=PG_USER,
+                password=PG_PASSWORD,
+                port=PG_PORT,
+                host=PG_HOST,
+                db=PG_DATABASE,
+                table_name=table_name_template,
+                csv_file=output_file_template
+            ),
+
+        )
+
+        wget_task >> ingest_task
+
+#For Yellow data
+yellow_data_dag = DAG(
+    "LocalIngestionDagForYellowData",
     schedule_interval = "0 6 2 * *",
-    start_date = datetime(2021,1,1)
+    start_date = datetime(2021,1,1),
+    catchup = True, 
+    max_active_runs= 3
 )
 
-URL_PREFIX = 'https://s3.amazonaws.com/nyc-tlc/trip+data/'
-URL_TEMPLATE = URL_PREFIX + 'yellow_tripdata_{{ execution_date.strftime(\"%Y-%m\") }}.csv'
-OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/output_{{ execution_date.strftime(\"%Y-%m\") }}.csv'
-TABLE_NAME_TEMPLATE = 'yellow_taxi_{{ execution_date.strftime(\"%Y-%m\") }}'
+YELLOW_URL_PREFIX = 'https://s3.amazonaws.com/nyc-tlc/trip+data/'
+YELLOW_URL_TEMPLATE = YELLOW_URL_PREFIX + 'yellow_tripdata_{{ execution_date.strftime(\"%Y-%m\") }}.csv'
+YELLOW_OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/yellow_taxi_{{ execution_date.strftime(\"%Y-%m\") }}.csv'
+YELLOW_TABLE_NAME_TEMPLATE = 'yellow_taxi_{{ execution_date.strftime(\"%Y-%m\") }}'
 
-
-with local_workflow:
-    wget_task = BashOperator(
-        task_id = 'wget',
-        bash_command = f'curl -sS {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}'
-
-    )
-    ingest_task = PythonOperator(
-        task_id = 'ingest',
-        python_callable = ingest_callable,
-        op_kwargs = dict(
-            user=PG_USER,
-            password=PG_PASSWORD,
-            port=PG_PORT,
-            host=PG_HOST,
-            db=PG_DATABASE,
-            table_name=TABLE_NAME_TEMPLATE,
-            csv_file=OUTPUT_FILE_TEMPLATE
-        ),
-
+download_transfer_dag(
+    dag = yellow_data_dag,
+    url_template = YELLOW_URL_TEMPLATE,
+    output_file_template = YELLOW_OUTPUT_FILE_TEMPLATE,
+    table_name_template = YELLOW_TABLE_NAME_TEMPLATE
     )
 
-    wget_task >> ingest_task
+
+#For FHV data
+fhv_data_dag = DAG(
+    "LocalIngestionDagForFhvData",
+    schedule_interval = "0 8 2 * *",
+    start_date = datetime(2019,1,1),
+    end_date=datetime(2020, 1, 1),
+    catchup = True,
+    max_active_runs= 3
+)
+
+
+FHV_URL_PREFIX = 'https://nyc-tlc.s3.amazonaws.com/trip+data/'
+FHV_URL_TEMPLATE = FHV_URL_PREFIX + 'fhv_tripdata_{{ execution_date.strftime(\"%Y-%m\") }}.csv'
+FHV_OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/fhv_{{ execution_date.strftime(\"%Y-%m\") }}.csv'
+FHV_TABLE_NAME_TEMPLATE = 'fhv_taxi_{{ execution_date.strftime(\"%Y-%m\") }}'
+
+download_transfer_dag(
+    dag = fhv_data_dag,
+    url_template = FHV_URL_TEMPLATE,
+    output_file_template = FHV_OUTPUT_FILE_TEMPLATE,
+    table_name_template = FHV_TABLE_NAME_TEMPLATE
+)
+
+
--- week_2_data_ingestion/airflow/dags/ingest_script.py
+++ week_2_data_ingestion/airflow/dags/ingest_script.py
@@ -12,9 +12,14 @@ def ingest_callable(user, password, port,host, db, table_name, csv_file):
     df_iter= pd.read_csv(csv_file, iterator = True, chunksize = 100000)
 
     df = next(df_iter)
-
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    if "yellow" in table_name:
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    elif "fhv":
+        df.pickup_datetime = pd.to_datetime(df.pickup_datetime)
+        df.dropoff_datetime = pd.to_datetime(df.dropoff_datetime)
+    else:
+        raise ValueError("Unrecognized table name")
 
 
     df.head(n=0).to_sql(name = table_name, con = engine, if_exists = 'replace')
@@ -23,8 +28,14 @@ def ingest_callable(user, password, port,host, db, table_name, csv_file):
         try:
             t_start = time()
             df = next(df_iter)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            if "yellow" in table_name:
+                df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+                df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            elif "fhv" in table_name:
+                df.pickup_datetime = pd.to_datetime(df.pickup_datetime, format='%Y-%m-%d %H:%M:%S')
+                df.dropoff_datetime = pd.to_datetime(df.dropoff_datetime, format='%Y-%m-%d %H:%M:%S')
+            else:
+                raise ValueError("Unrecognized table name")
             df.to_sql(name =table_name, con = engine, if_exists = 'append')
             t_end = time()
             print('inserted another chunk..., took %.3f second' % (t_end-t_start))
